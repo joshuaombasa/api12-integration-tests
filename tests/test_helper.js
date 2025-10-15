4a1c176338ea484e3eeec8ba735e14e68b07888d
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2025-10-15 23:26:07 +0300

    Update test_helper.js

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,26 +1,41 @@
 const Provider = require('../models/provider')
 
 const providersData = [
-  { name: 'provider1', licenced: true },
-  { name: 'provider2', licenced: false },
-  { name: 'provider3', licenced: true },
-  { name: 'provider4', licenced: false },
-  { name: 'provider5', licenced: true }
+  { name: 'Provider 1', licenced: true },
+  { name: 'Provider 2', licenced: false },
+  { name: 'Provider 3', licenced: true },
+  { name: 'Provider 4', licenced: false },
+  { name: 'Provider 5', licenced: true },
 ]
 
-const validProviderData = { name: 'provider6', licenced: true }
-const inValidProviderData = { name: 'provider7', licence: true }
+const validProviderData = { name: 'Provider 6', licenced: true }
+const invalidProviderData = { name: 'Provider 7', licence: true } // Typo intentional for test
 
-const providersInDb = async () => {
-  const providerData = await Provider.find({})
-  return providerData.map(provider => provider.toJSON())
+/**
+ * Fetch all providers currently stored in the database.
+ * @returns {Promise<Array>} - List of providers as plain JSON objects.
+ */
+const getProvidersInDb = async () => {
+  const providers = await Provider.find({})
+  return providers.map(provider => provider.toJSON())
 }
 
-const nonExistentId = async () => {
-  const providerObject = new Provider({ name: 'provider8', licenced: true })
-  const savedProvider = await providerObject.save()
+/**
+ * Generate a valid but non-existent provider ID.
+ * Useful for testing endpoints that handle unknown IDs.
+ * @returns {Promise<string>} - A valid MongoDB ObjectId string that no longer exists in the DB.
+ */
+const getNonExistentId = async () => {
+  const provider = new Provider({ name: 'Temporary Provider', licenced: true })
+  const savedProvider = await provider.save()
   await Provider.findByIdAndDelete(savedProvider._id)
   return savedProvider._id.toString()
 }
 
-module.exports = { providersData, validProviderData, inValidProviderData, providersInDb , nonExistentId}
+module.exports = {
+  providersData,
+  validProviderData,
+  invalidProviderData,
+  getProvidersInDb,
+  getNonExistentId,
+}
