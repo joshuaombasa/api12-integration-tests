ec7b2d0517a1efccae060457a5285ba5a7c86a69
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2025-10-19 23:35:55 +0300

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -3,58 +3,73 @@ const Provider = require('../models/provider')
 
 const providerRouter = express.Router()
 
-providerRouter.get('/', async (request, response, next) => {
-  const providers = await Provider.find({})
+// GET all providers
+providerRouter.get('/', async (req, res, next) => {
   try {
-    response.send(providers)
+    const providers = await Provider.find({})
+    res.status(200).json(providers)
   } catch (error) {
     next(error)
   }
 })
 
-providerRouter.get('/:id', async (request, response, next) => {
+// GET provider by ID
+providerRouter.get('/:id', async (req, res, next) => {
   try {
-    const provider = await Provider.findById(request.params.id)
+    const provider = await Provider.findById(req.params.id)
     if (!provider) {
-      return response.status(404).end()
+      return res.status(404).json({ message: 'Provider not found' })
     }
-    response.send(provider)
+    res.status(200).json(provider)
   } catch (error) {
     next(error)
   }
 })
 
-providerRouter.post('/', async (request, response, next) => {
-  const { name, licenced } = request.body
+// CREATE new provider
+providerRouter.post('/', async (req, res, next) => {
   try {
-    const providerObject = new Provider({ name, licenced })
-    const savedProvider = await providerObject.save()
-    response.status(201).send(savedProvider)
+    const { name, licenced } = req.body
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required' })
+    }
+
+    const newProvider = new Provider({ name, licenced })
+    const savedProvider = await newProvider.save()
+    res.status(201).json(savedProvider)
   } catch (error) {
     next(error)
   }
 })
 
-
-providerRouter.put('/', async (request, response, next) => {
-  const { name, licenced } = request.body
+// UPDATE provider by ID
+providerRouter.put('/:id', async (req, res, next) => {
   try {
+    const { name, licenced } = req.body
     const updatedProvider = await Provider.findByIdAndUpdate(
-      request.params.id,
+      req.params.id,
       { name, licenced },
-      { new: true }
+      { new: true, runValidators: true }
     )
-    response.send(updatedProvider)
+
+    if (!updatedProvider) {
+      return res.status(404).json({ message: 'Provider not found' })
+    }
+
+    res.status(200).json(updatedProvider)
   } catch (error) {
     next(error)
   }
 })
 
-providerRouter.delete('/:id', async (request,response,next) => {
- 
+// DELETE provider by ID
+providerRouter.delete('/:id', async (req, res, next) => {
   try {
-    await Provider.findByIdAndDelete(request.params.id)
-    response.sendStatus(204)
+    const deletedProvider = await Provider.findByIdAndDelete(req.params.id)
+    if (!deletedProvider) {
+      return res.status(404).json({ message: 'Provider not found' })
+    }
+    res.sendStatus(204)
   } catch (error) {
     next(error)
   }
